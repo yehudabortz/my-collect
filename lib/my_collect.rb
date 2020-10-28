488e240c2ad48eef9a3fece0963bea0eb9702c38
Yehuda Bortz <yehudabortz@icloud.com>
2020-10-28 11:04:55 -0500

    commit tree 886594c1dc6eb22cf996cf6986624feb62da15ec

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,18 @@
+require 'pry'
+languages= ['ruby', 'javascript', 'python', 'objective-c']
+def my_collect(languages)
+    new_collection = []
+    i = 0
+    while i < languages.length
+    new_collection << yield(languages[i])
+    i += 1
+    end
+# binding.pry
+    new_collection
+end
+my_collect(languages) do |n|
+    puts "#{n.upcase}"
+end
+
 
 
